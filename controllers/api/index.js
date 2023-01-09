2b0454172ea5711253b4d0f2904a12407bbd208a
cmarielorber <109984761+cmarielorber@users.noreply.github.com>
2023-01-09 14:00:33 -0800

    debugged daschboard-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postroutes')
-const commentRoutes = require('./commentroutes')
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes')
+const commentRoutes = require('./comment-routes')
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
