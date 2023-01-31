d6e179e7d836fde91f5b7e7264ab3e3bc3f2bf76
cmarielorber <109984761+cmarielorber@users.noreply.github.com>
2023-01-31 14:24:19 -0800

    Revert "added helpers"
    This reverts commit 67b5a2666ead5ab8ec3e7f82f0c156885b478ff3.

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,5 @@
 module.exports = {
+<<<<<<< HEAD
   format_date: (date) => {
     console.log(date);
     return new Date(date).toLocaleDateString();
@@ -11,3 +12,26 @@ module.exports = {
     return comments.length;
   },
 };
+=======
+    format_date: (date) => {
+        //format date as MM/DD/YYYY
+        return date.toLocaleDateString();
+    },
+    format_amount: (amount) => {
+        //format large numbers with commas
+        return parseInt(amount).toLocaleString();
+    },
+    get_emoji: () => {
+        const randomNum = Math.random();
+
+        //return a random emoji
+        if (randomNum > 0.7) {
+            return `<span for="img" aria-label="lightbulb">💡</span>`;
+        } else if (randomNum > 0.4) {
+            return `<span for="img" aria-label="laptop">💻</span>`;
+        } else {
+            return `<span for="img" aria-label="gear">⚙️</span>`;
+        }
+    },
+};
+>>>>>>> parent of 67b5a26 (added helpers)
