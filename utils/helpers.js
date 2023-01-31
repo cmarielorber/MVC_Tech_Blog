4b3bd8f09ab9adf8d9f6b4c88a54f03be4c09d18
cmarielorber <109984761+cmarielorber@users.noreply.github.com>
2023-01-31 14:27:39 -0800

    revert changes

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,22 @@
 module.exports = {
-  format_date: (date) => {
-    console.log(date);
-    return new Date(date).toLocaleDateString();
-    //   return `${new Date(date).getMonth() + 1}/${new Date(
-    //     date
-    //   ).getDate()}/${new Date(date).getFullYear()}`;
-  },
+    format_date: (date) => {
+        //format date as MM/DD/YYYY
+        return date.toLocaleDateString();
+    },
+    format_amount: (amount) => {
+        //format large numbers with commas
+        return parseInt(amount).toLocaleString();
+    },
+    get_emoji: () => {
+        const randomNum = Math.random();
 
-  commentLengths: (comments) => {
-    return comments.length;
-  },
+        //return a random emoji
+        if (randomNum > 0.7) {
+            return `<span for="img" aria-label="lightbulb">💡</span>`;
+        } else if (randomNum > 0.4) {
+            return `<span for="img" aria-label="laptop">💻</span>`;
+        } else {
+            return `<span for="img" aria-label="gear">⚙️</span>`;
+        }
+    },
 };
-
- 
